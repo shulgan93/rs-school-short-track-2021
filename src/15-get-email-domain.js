65c02c935f9f5c63be449be4291a22018c025c8e
shulgan93 <mr.shulhan93@gmail.com>
2021-04-09 22:54:18 +0300

    feat: completed task

--- src/15-get-email-domain.js
+++ src/15-get-email-domain.js
@@ -8,8 +8,9 @@
  * For the input 'prettyandsimple@example.com', the output should be 'example.com'
  *
  */
-function getEmailDomain(/* email */) {
-  throw new Error('Not implemented');
+function getEmailDomain(email) {
+  const dog = email.lastIndexOf('@') + 1;
+  return email.slice(dog);
 }
 
 module.exports = getEmailDomain;
