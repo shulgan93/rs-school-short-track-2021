2113ad452c08d6cc8c27253101c83e706f8a3e90
shulgan93 <mr.shulhan93@gmail.com>
2021-04-10 11:13:37 +0300

    feat: completed task

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -12,8 +12,9 @@
  * For 00-1B-63-84-45-E6, the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const arr = n.split('-');
+  return arr.every((el) => /^[0-9A-F][0-9A-F]$/g.test(el));
 }
 
 module.exports = isMAC48Address;
--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,24 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  let count = 1;
+
+  const arr = str.split('').reduce((acc, el, idx, copyArr) => {
+    if (el !== copyArr[idx + 1] && count === 1) {
+      acc.push(el);
+      return acc;
+    }
+    if (el === copyArr[idx + 1]) {
+      count++;
+      return acc;
+    }
+    acc.push(count + el);
+    count = 1;
+    return acc;
+  }, []);
+
+  return arr.join('');
 }
 
 module.exports = encodeLine;
